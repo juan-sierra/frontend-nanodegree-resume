f366dc0fe98c65b3edc21fb77974072a64f9f8f0
Juan Sierra <jasierra.garcia@gmail.com>
2017-03-14 13:59:43 -0400

    Creating objects for data

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,34 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio = {
+    "name" : "Juan Sierra",
+    "role" : "Front End Developer",
+    "contacts" : {
+        "mobile" : "571-571-5709",
+        "email" : "jasierra.garcia@gmail.com",
+        "twitter" : "jasierragarcia",
+        "github" : "juan-sierra",
+        "location" : "San Pedro Sula, Honduras"
+    },
+    "welcomeMessage" : "Thank you for visiting!",
+    "skills" : [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "Gulp",
+        "Linux",
+        "Bash / ZSH"
+    ],
+    "biopic" : "images/me.jpg"
+}
+
+
+// bio component
+bio.display = function () {
+    var formattedName = HTMLheaderName.replace("%data%", bio.name);
+    $("#header").append(formattedName);
+    
+    var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+    $("#header").append(formattedRole);
+
+};
+
+bio.display();
